36bdfed0d06b4ebdbc3e3e7f401926e84948e89f
Gabriel <gabriel.yonamine@edu.uniso.br>
2020-10-27 19:30:14 -0300

    primeiroCommit

---
+++ app/src/main/java/com/example/applojamobile/Frag3.java
@@ -0,0 +1,19 @@
+package com.example.applojamobile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag3 extends Fragment {
+    //Código para retornar o layout do fragment 3
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag3_layout, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/applojamobile/FragmentoLogin.java
@@ -0,0 +1,19 @@
+package com.example.applojamobile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentoLogin extends Fragment {
+    //Código para retornar o layout do login
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.telalogin, container, false);
+    }
+}
